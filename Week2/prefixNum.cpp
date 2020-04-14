368408f26d26e54134f3f3784e74b46968cd0d1c
c-thun <42250009+c-thun@users.noreply.github.com>
2020-04-14 23:10:05 +0530

    Update prefixNum.cpp

--- Week2/prefixNum.cpp
+++ Week2/prefixNum.cpp
@@ -1,34 +1,34 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+void exec() {
+	long long x,y,a,f=0,lim,curr;
+	lim=pow(10,18);
+	cin>>x>>y;
+	for(long long i=1;!f;i*=10) {
+	    a = i*x;
+		if(a>lim/10) f=1;
+        curr=a%y;
+        if(curr==0)  {
+		    cout<<a<<endl;
+		    return;
+		}
+		if(y-curr < i) {
+			a=a+y-curr;
+			if(a>0 and a<=lim) {
+				cout<<a<<endl;
+				return;
+			}
+		} 
+	}
+	cout<<"-1\n";
+	return;
+}
+
 int main() {
-    int tc;
-    cin >> tc;
-    for(int i=0;i<tc;i++) {
-        long long int x, y;
-        cin>>x>>y;
-        long long int nums=1, ans=-1; int flag=0;
-        while(true) {
-            long long int rem = (y-(x%y))%y;
-            if(rem<nums) {
-                ans = x+rem;
-            }
-            if(ans<1e18 and (ans%y)==0) {
-                break;
-            }
-            if(x>1e18) {
-                flag = 1;
-                break;
-            }
-            else {
-                x*=10;
-                nums*=10;
-            }
-        }
-        if(ans<=1e18 and flag==0) 
-            cout<<ans<<"\n";
-        else 
-            cout<<-1<<"\n";
+    int t;
+    cin>>t;
+    while(t--) {
+        exec();
     }
-  return 0;
 }
